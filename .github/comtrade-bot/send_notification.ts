de44c9280d2367de5e9cf144fba2ab3ea2c484d2
Josephxrex <88760498+Josephxrex@users.noreply.github.com>
2023-08-08 08:09:25 -0600

    Update send_notification.ts

--- .github/comtrade-bot/send_notification.ts
+++ .github/comtrade-bot/send_notification.ts
@@ -3,20 +3,19 @@ const axios = require('axios');
 async function sendTelegramNotification() {
   const botToken = process.env.TELEGRAM_TOKEN;
   const chatId = process.env.TELEGRAM_TO;
+  const message= process.env.TELEGRAM_MESSAGE;
   const messageThreadId = process.env.TELEGRAM_THREAD_ID;
   const githubServerUrl = process.env.GITHUB_SERVER_URL;
   const githubRepository = process.env.GITHUB_REPOSITORY;
   const pullRequestNumber = process.env.PULL_REQUEST_NUMBER;
    
-  const prUrl = `${githubServerUrl}/${githubRepository}/pull/${pullRequestNumber}`;
-  const message = `Nueva Pull Request abierta: ${prUrl}`;
 
   const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
   const payload = {
     chat_id: chatId,
     text: message,
     message_thread_id: messageThreadId,
-    parse_mode: 'HTML',
+    parse_mode: 'Markdown',
   };
 
   try {
@@ -28,7 +27,7 @@ async function sendTelegramNotification() {
     console.error(
       `An error occurred while sending the Telegram message: ${error}`
     );
-    throw err;
+    throw error;
   }
 }
 
